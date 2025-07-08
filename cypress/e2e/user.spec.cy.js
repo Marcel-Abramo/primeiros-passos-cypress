72114de854f46c173eb14eef02c062ac641e0a35
Marcel-Abramo <marcel.abramo.galego@gmail.com>
2025-07-07 21:17:19 -0300

    Add page objects

--- cypress/e2e/user.spec.cy.js
+++ cypress/e2e/user.spec.cy.js
@@ -1,53 +1,118 @@
 import userData from '../fixtures/user-data.json'
+import LoginPage from '../pages/loginPage'
+import DashboardPage from '../pages/dashboardPage'
+import MenuPage from '../pages/menuPage'
+
+
+const loginPage = new LoginPage()
+const dashboardPage = new DashboardPage()
+const menuPage = new MenuPage()
 
 describe('Orange HRM Tests', () => {
 
   const selectorsList = {
-    usernameField: "[name='username']",
-    passwordField: "[name='password']",
-    loginButton: "[type='submit']",
-    selectionTitleTopBar: '.oxd-topbar-header-breadcrumb-module',
-    dashboardGrid: '.orangehrm-dashboard-grid',
-    wrongCredentialAlert: "[role='alert']",
-    myInfoButton: '[href="/web/index.php/pim/viewMyDetails"]',
+   
+   
     firstNameField: "[name='firstName']",
     lastNameField: "[name='lastName']",
     genericField: ".oxd-input--active",
     dateField: "[placeholder='yyyy-mm-dd']",
     dateCloseButton: ".--close",
     submitButton: "[type='submit']",
-
-
-
+    selectText: '.oxd-select-text--arrow',
+    selectNationality: '.oxd-select-dropdown > :nth-child(27)',
+    selectMaritalStatus: '.oxd-select-dropdown > :nth-child(3)',
+    selectGender: '.oxd-radio-input',
   }
   
   
   it.only('User Info Update - Success', () => {
-    cy.visit('/auth/login')
-    cy.get(selectorsList.usernameField).type(userData.userSuccess.username)
-    cy.get(selectorsList.passwordField).type(userData.userSuccess.password)
-    cy.get(selectorsList.loginButton).click()
-    cy.location('pathname').should('equal', '/web/index.php/dashboard/index')
-    cy.get(selectorsList.dashboardGrid)
-    cy.get(selectorsList.myInfoButton).click()
+    loginPage.accessLoginPage() 
+    loginPage.loginWithUser(userData.userSuccess.username, userData.userSuccess.password)
+
+    dashboardPage.checkDashboardPage()
+
+    menuPage.accessMyInfo()
+    
+
+
+    
+    
     cy.get(selectorsList.firstNameField).clear().type('FirstNameTest')
     cy.get(selectorsList.lastNameField).clear().type('LastNameTest')
-    cy.get(selectorsList.genericField).eq(3).clear().type('NicknameTest')
-    cy.get(selectorsList.genericField).eq(4).clear().type('Employee')
-    cy.get(selectorsList.genericField).eq(5).clear().type('OtherIdTest')
-    cy.get(selectorsList.genericField).eq(6).clear().type('DriversLicenseTest') 
-    cy.get(selectorsList.genericField).eq(7).clear().type('2025-03-10')  
+    //cy.get(selectorsList.genericField).eq(3).clear().type('NicknameTest') RETIRAR
+     
+    cy.get(selectorsList.genericField).eq(3).clear().type('Employee')
+    cy.get(selectorsList.genericField).eq(4).clear().type('OtherIdTest')
+    cy.get(selectorsList.genericField).eq(5).clear().type('LicenseTest')
+    cy.get(selectorsList.genericField).eq(6).clear().type('2025-03-10')  
     cy.get(selectorsList.dateCloseButton).click()
-    cy.get(selectorsList.genericField).eq(8).clear().type('ssnNumberTest')
-    cy.get(selectorsList.genericField).eq(9).clear().type('sinNumberTest')
-    cy.get(selectorsList.submitButton).eq(0).click()
-    cy.get('body').should('contain', 'Successfully Updated')
-    cy.get('.oxd-toast-close')
+    //cy.get(selectorsList.genericField).eq(7).clear().type('ssnNumberTest') RETIRAR
+    //cy.get(selectorsList.genericField).eq(8).clear().type('sinNumberTest') RETIRAR
+    
+    //Nationality
+    cy.get(selectorsList.selectText).eq(0).click() //aqui a caixa abriu com o click
+    cy.get(selectorsList.selectNationality).click() //aqui seleciona a nacionalidade número 27 da lista
+    
+    //Marital Status
+    cy.get(selectorsList.selectText).eq(1).click() //aqui a caixa abriu com o click
+    cy.get(selectorsList.selectMaritalStatus).click() //aqui seleciona o status número 3 da lista
+   
+    //Date of Birth
+    cy.get(selectorsList.genericField).eq(8).clear().type('2025-08-08')  
+    cy.get(selectorsList.dateCloseButton).click()
 
+    //Gender
+    cy.get(selectorsList.selectGender).eq(0).click() 
+    
+    
+
+
+    //cy.get('.oxd-select-dropdown').scrollIntoView() //aqui a caixa está aberta TESTE RETIRAR
+    //cy.contains('li', 'Brazilian').click() TESTE RETIRAR
+    //cy.wait(2000)
+    
+       
+    
+    cy.get(selectorsList.submitButton).eq(0).click({force: true })
+    cy.get('body').should('contain', 'Successfully Update')
+    cy.get('.oxd-toast-close')
+    
+    
+
+   
 
 
 
   })
+
+
+it('sets Brazilian', () => {
+  // https://on.cypress.io/select
+  // set using text
+  // <option value="MA">Massachusetts</option>
+  cy.get('#my-state').select('Massachusetts')
+
+  // confirm the selected value
+  cy.get('#my-state').should('have.value', 'MA')
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
   it('Login Fail', () => {
     cy.visit('/auth/login')
